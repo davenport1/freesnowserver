cbc302ded49fe41d9261f56a5c65391916b9a8fb
davenport1 <61665447+davenport1@users.noreply.github.com>
2024-03-13 19:49:49 -0700

    docker persistent voluime and initialize script fix

--- docker/init.sql
+++ docker/init.sql
@@ -1,8 +1,17 @@
--- CREATE DATABASE
-CREATE DATABASE freesnow_db;
+-- Check if the database exists before attempting to create it
+SELECT datname FROM pg_database WHERE datname = 'freesnow_db';
+
+-- If the database does not exist, create it
+DO $$BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'freesnow_db') THEN
+    CREATE DATABASE freesnow_db;
+END IF;
+END$$;
+-- SWITCH TO DATABASE
+\c freesnow_db
 
 -- CREATE TABLES
-CREATE TABLE ski_resort (
+CREATE TABLE IF NOT EXISTS ski_resort (
     id SERIAL PRIMARY KEY,
     resort_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -11,38 +20,45 @@ CREATE TABLE ski_resort (
     version INT
 );
 
-CREATE TABLE snow_report (
+CREATE TABLE IF NOT EXISTS snow_report (
     id SERIAL PRIMARY KEY,
-    ski_resort_id INT,
+    ski_resort_id INT REFERENCES ski_resort(id),
     last_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     _24_hr INT,
     _72_hr INT,
     week INT
 );
 
-CREATE TABLE trail (
-    id INT,
-    ski_resort_id INT,
+CREATE TABLE IF NOT EXISTS trail (
+    id SERIAL PRIMARY KEY,
+    ski_resort_id INT REFERENCES ski_resort(id),
     name VARCHAR(255) NOT NULL,
     difficulty INT,
     status INT,
     last_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE lift (
-    id INT,
-    ski_resort_id INT,
+CREATE TABLE IF NOT EXISTS lift (
+    id SERIAL PRIMARY KEY,
+    ski_resort_id INT REFERENCES ski_resort(id),
     name VARCHAR(255) NOT NULL,
     status INT,
     last_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE week_forecast(
-    id
-)
-
-CREATE TABLE day_forecast(
-
-)
+CREATE TABLE IF NOT EXISTS weather_forecast_day(
+    id SERIAL PRIMARY KEY,
+    ski_resort_id INT REFERENCES ski_resort(id),
+    temperature_high INT,
+    temperature_low INT,
+    wind_direction INT,
+    wind_speed_mph INT,
+    overcast_level INT,
+    humidity_percentage INT,
+    temperature_feels_like INT,
+    temperature_wind_chill INT,
+    sunrise TIMESTAMPTZ,
+    sunset TIMESTAMPTZ
+);
 
 -- CREATE RELATIONSHIPS
